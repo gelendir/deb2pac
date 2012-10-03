438cb8b557291a9d00e19cf2453ffff123019a41
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-10-03 14:10:52 -0400

    extract files from ar archive

--- ar.py
+++ ar.py
@@ -1,28 +1,51 @@
-import sys
-from pprint import pprint
+import os
 
 MAGIC_STR = "!<arch>\n"
 MAGIC_LEN = len(MAGIC_STR)
 
 def extract_file_header(f):
 
-    sizes = [
-        ('name', 16),
-        ('timestamp', 12),
-        ('owner_id', 6),
-        ('group_id', 6),
-        ('file_mode', 8),
-        ('file_size', 10),
-        ('file_magic', 2),
-    ]
+    header = {
+        'name'          : f.read(16).strip(),
+        'timestamp'     : int(f.read(12).strip()),
+        'owner_id'      : int(f.read(6).strip()),
+        'group_id'      : int(f.read(6).strip()),
+        'file_mode'     : int(f.read(8).strip()),
+        'size'          : int(f.read(10).strip()),
+        'file_magic'    : f.read(2),
+    }
 
-    header = dict( (key, f.read(size)) for (key, size) in sizes )
+    if header['file_magic'] != "\x60\x0a":
+        raise Exception("invalid file magic %s" % header['file_magic'])
 
     return header
 
+def extract_file(archive, header, outdir):
+
+    path = os.path.join(outdir, header['name'])
+
+    with open(path, 'w') as f:
+        data = archive.read(header['size'])
+        f.write(data)
+
+def extract_archive(filepath, outdir):
+
+    filesize = os.path.getsize(filepath)
+
+    with open(filepath, 'rb') as f:
+
+        magic = f.read(MAGIC_LEN)
+        if magic != MAGIC_STR:
+            raise Exception("%s does not seem to be a valid ar archive" % filepath)
+
+        while f.tell() < filesize:
+            header = extract_file_header(f)
+            extract_file(f, header, outdir)
+
+            if header['size'] % 2 == 1:
+                f.read(1)
+
 if __name__ == "__main__":
-
-    with open(sys.argv[1], 'rb') as f:
-        f.read(MAGIC_LEN)
-        pprint(extract_file_header(f))
-
+    import sys
+    filepath = sys.argv[1]
+    extract_archive(filepath, ".")
